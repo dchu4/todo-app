85fba66dec0da10accad8f967afe643d60dd9f33
Daniel Chu <danchu123@gmail.com>
2016-06-24 22:13:52 -0500

    angularjs todo app

---
+++ app/assets/javascripts/todo.js
@@ -0,0 +1,55 @@
+(function() {
+  "use strict";
+
+  angular.module("app").controller("todoController",function($scope){
+    $scope.tasks = [{ taskName: "Task 1", complete: false}, { taskName: "Task 2", complete: false}, { taskName: "Task 3", complete: false}];
+    // $scope.incomplete = 3;
+
+    $scope.addTask = function(newTaskName){
+      if(newTaskName !== ""){
+        var taskObject = {taskName: newTaskName, complete: false};
+
+        $scope.tasks.push(taskObject);
+        $scope.addNewTask = "";
+      }
+      else{
+        alert("Add Text");
+      }
+    };
+
+    $scope.toggleComplete = function(task){
+      if(!task.complete){
+        task.complete = !task.complete;
+      }
+      else{
+        task.complete = !task.complete;
+      }
+    };
+
+    $scope.clearTask = function(index){
+      $scope.tasks.splice(index,1);
+    };
+
+    $scope.incompleteTasks = function(){
+      var incomplete = 0;
+
+      for(var i = 0; i < $scope.tasks.length; i++){
+        if($scope.tasks[i].complete == false){
+          incomplete++;
+        }
+      }
+      return incomplete;
+    }
+
+    $scope.deleteComplete = function(){
+      for(var i = 0; i < $scope.tasks.length; i++){
+        if($scope.tasks[i].complete == true){
+          $scope.clearTask(i);
+          i--;
+        }
+      }
+    }
+
+  });
+
+}());
